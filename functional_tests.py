48e740d4629363fed427f55aa6f33736e1307768
Eitan Borvick <eborvick@gmail.com>
2017-04-09 23:14:43 +0300

    First FT specced out in comments, now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		
+	def tearDown(self):
+		self.browser.quit()
+		
+	def test_can_start_a_list_and_retrieve_it_later(self):
+
+		#Lonni heard about a new online to-do app
+		#She goes to check out it's homepage
+		self.browser.get('http://localhost:8000')
+
+		#She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+		#She is invited to enter a to-do item straight away
+
+		#She types "Buy paper hearts" into a text box
+
+		#When she hits enter, the page updates, and now the page lists:
+		# "1: Buy paper hearts" as an item in a to-do list
+
+		#There is still a text boc inviting ger to add another item
+		#She enters "Use paper hearts in scrapbook"
+
+		#The page updates again, and now shows both items on her list
+
+		#Lonni wonder whether the site will remember her lists
+		#Then she sees the site has generated a unique URL for her
+		#There is some explanatory text to that effect
+
+		#She visits that URL - her to-do list is still there
+
+		#Satisfied, she goes to sleep
+
+if __name__ == '__main__':
+	unittest.main()
