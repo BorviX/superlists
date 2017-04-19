fb472ab3d124c146058b533e07a9d20dcc97a927
Eitan Borvick <eborvick@gmail.com>
2017-04-19 18:03:25 +0300

    Added function to FT that checks if text is in table

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,13 @@ class NewVisitorTest(unittest.TestCase):
 		
 	def tearDown(self):
 		self.browser.quit()
-		
+	
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+	
+	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		#Lonni heard about a new online to-do app
@@ -37,10 +43,7 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy paper hearts" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy paper hearts', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy paper hearts')
 
 		#There is still a text box inviting her to add another item
 		#She enters "Use paper hearts in scrapbook" and presses enter
@@ -50,10 +53,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		#The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy paper hearts', [row.text for row in rows])
-		self.assertIn('2: Use paper hearts in scrapbook', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy paper hearts')
+		self.check_for_row_in_list_table('2: Use paper hearts in scrapbook')
 		
 		#Lonni wonder whether the site will remember her lists
 		#Then she sees the site has generated a unique URL for her
