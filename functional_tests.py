72642e3a4eb1d610d4c528ddef7423fbb1d09946
Eitan Borvick <eborvick@gmail.com>
2017-04-19 17:55:22 +0300

    Updated HTML and tests to show one item correctly in table

--- functional_tests.py
+++ functional_tests.py
@@ -40,20 +40,25 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy paper hearts' for row in rows), "New to-do item did not appear in table"
-		)
-		
-		#There is still a text box inviting ger to add another item
-		#She enters "Use paper hearts in scrapbook"
-		self.fail('Finish the test!')
+		self.assertIn('1: Buy paper hearts', [row.text for row in rows])
+
+		#There is still a text box inviting her to add another item
+		#She enters "Use paper hearts in scrapbook" and presses enter
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use paper hearts in scrapbook')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		#The page updates again, and now shows both items on her list
-
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy paper hearts', [row.text for row in rows])
+		self.assertIn('2: Use paper hearts in scrapbook', [row.text for row in rows])
+		
 		#Lonni wonder whether the site will remember her lists
 		#Then she sees the site has generated a unique URL for her
 		#There is some explanatory text to that effect
-
+		self.fail('Finish the test!')
 		#She visits that URL - her to-do list is still there
 
 		#Satisfied, she goes to sleep
