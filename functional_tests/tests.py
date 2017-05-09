ad4004f23293ef67624eef7947bd7c2e0145d8f5
Eitan Borvick <eborvick@gmail.com>
2017-05-09 21:35:12 +0300

    Added multiple users having different urls to FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,8 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 	
-	def test_can_start_a_list_and_retrieve_it_later(self):
-
+	def test_can_start_a_list_for_one_user(self):
 		#Lonni heard about a new online to-do app
 		#She goes to check out it's homepage
 		self.browser.get(self.live_server_url)
@@ -52,7 +51,6 @@ class NewVisitorTest(LiveServerTestCase):
 		#When she hits enter, the page updates, and now the page lists:
 		# "1: Buy paper hearts" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 		self.wait_for_row_in_list_table('1: Buy paper hearts')
 
 		#There is still a text box inviting her to add another item
@@ -60,16 +58,57 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use paper hearts in scrapbook')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
 		#The page updates again, and now shows both items on her list
 		self.wait_for_row_in_list_table('1: Buy paper hearts')
 		self.wait_for_row_in_list_table('2: Use paper hearts in scrapbook')
-		
-		#Lonni wonder whether the site will remember her lists
-		#Then she sees the site has generated a unique URL for her
-		#There is some explanatory text to that effect
-		self.fail('Finish the test!')
-		#She visits that URL - her to-do list is still there
 
 		#Satisfied, she goes to sleep
+
+	def test_multiple_users_can_start_list_at_different_urls(self):
+		#Lonni starts a new todo list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy paper hearts')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy paper hearts')
+		
+		#She notices that her list has a unique URL
+		lonni_list_url = self.browser.current_url
+		self.assertRegex(lonni_list_url, '/lists/.+')
+		
+		#Now, a new user, Avichai, comes along to the site
+		
+		## We use a new browser session to make sure that no information of Lonni's is coming through from cookies etc.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		
+		#Avichai visits the home page. There is no sign of Lonni's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy paper hearts', page_text)
+		self.asserNotIn('paper hearts in scrapbook', page_text)
+		
+		#Avichai starts a new list by entering a new item.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Play with bimba')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Play with bimba')
+		
+		#Avichai gets his own unique URL
+		avichai_list_url = self.browser.current_url
+		self.assertRegex(avichai_list_url, '/lists/.+')
+		self.assertNotEqual(avichai_list_url, lonni_list_url)
+		
+		#Again, there is no trace of Lonni's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy paper hearts', page_text)
+		self.asserNotIn('paper hearts in scrapbook', page_text)
+		
+		#Satisfied they all go for a nap
+		
+		
+		
+		
+		
+		
